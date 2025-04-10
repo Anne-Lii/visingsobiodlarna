1116b2bc0d8a8d404afa9dfe91e38b8766a3daa2
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-10 10:12:27 +0200

    implementerat routing med layout komponenten, lagt till navigering i headern med Navlink

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
 import './App.scss';
-import Header from './components/Header';
+
 
 
 function App() {
   return (
-    <div className="App">
-       <Header />
-     
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => {
     return (
         <header>
-            
+            <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Startsida</NavLink>
+            <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>Logga in</NavLink>            
         </header>
     );
 

043dc629db94dacd0b6eaad5050576f4a6d09a1e
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-10 11:02:31 +0200

    lagt till logotyp i headern och placerat länkar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,37 @@
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
         <header>
             <nav>
-                <ul>
-                    <li>
-                    <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Startsida</NavLink>
+                <div className="logo">
+                    <NavLink to="/" className="logo-link">
+                        <img src={logo} alt="Logotyp bee" />
+                    </NavLink>
+                </div>
+                <ul className="nav-list">
+                    <li className="menu-link">
+                        <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>
+                            Startsida
+                        </NavLink>
                     </li>
-                    <li>
-                    <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>Logga in</NavLink>   
+                    <li className="menu-link">
+                        <NavLink to="/news" className={({ isActive }) => isActive ? 'active' : ''}>
+                            Nyheter
+                        </NavLink>
+                    </li>
+                    <li className="menu-link login-link">
+                        <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>
+                            Logga in
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
-            
-                     
         </header>
-    );
 
-}
+    );
+};
 
 export default Header;

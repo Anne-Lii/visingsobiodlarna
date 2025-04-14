28384adc1dd6ee37e1279b7bcd3d46bb5f46446d
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-14 08:54:10 +0200

    lagt till sida för registrering med input för förnamn, efternamn, epost och lösenord

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,36 @@
+import '../pages/Register.scss'
 
 const Register = () => {
-  return (
-    <div>
-        <h1>Registrera ny användare</h1>      
-    </div>
-  )
+    return (
+        <div>
+            <h1>Registrera ny användare</h1>
+
+            <form aria-labelledby="register-form" className="register-form">
+                <div>
+                    <label htmlFor="firstName">Förnamn</label>
+                    <input type="text" id="firstName" name="firstName" required/>
+                </div>
+                <div>
+                    <label htmlFor="lastName">Efternamn</label>
+                    <input type="text" id="lastName" name="lastName" required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">E-postadress</label>
+                    <input type="email" id="email" name="email" required/>
+                </div>
+                <div>
+                    <label htmlFor="password">Lösenord</label>
+                    <input type="password" id="password" name="password" required/>
+                </div>
+
+                <div>
+                    <button type="submit" className='register_button'>Registrera</button>
+                </div>
+            </form>
+
+        </div>
+    )
 }
 
 export default Register

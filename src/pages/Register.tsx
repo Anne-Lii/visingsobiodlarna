1b7e8ab61c0414d30f8637a985d1e3130bd2c2b6
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-14 09:05:19 +0200

    lagt till hantering av formulärdata med useState och handleChange på registreringssidan

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,32 @@
+import { useState } from 'react'
 import '../pages/Register.scss'
 
+interface RegisterFormData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
 const Register = () => {
+
+    const [formData, setFormData] = useState<RegisterFormData>({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+
+    });
+
+    //Hanterar ändringar i formuläret
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
     return (
         <div>
             <h1>Registrera ny användare</h1>
@@ -8,20 +34,23 @@ const Register = () => {
             <form aria-labelledby="register-form" className="register-form">
                 <div>
                     <label htmlFor="firstName">Förnamn</label>
-                    <input type="text" id="firstName" name="firstName" required/>
+                    <input type="text" id="firstName" name="firstName" value={formData.firstName}
+                        onChange={handleChange} required/>
                 </div>
                 <div>
                     <label htmlFor="lastName">Efternamn</label>
-                    <input type="text" id="lastName" name="lastName" required
+                    <input type="text" id="lastName" name="lastName" value={formData.lastName}
+                        onChange={handleChange} required
                     />
                 </div>
                 <div>
                     <label htmlFor="email">E-postadress</label>
-                    <input type="email" id="email" name="email" required/>
+                    <input type="email" id="email" name="email" value={formData.email}
+                        onChange={handleChange} required/>
                 </div>
                 <div>
                     <label htmlFor="password">Lösenord</label>
-                    <input type="password" id="password" name="password" required/>
+                    <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required/>
                 </div>
 
                 <div>

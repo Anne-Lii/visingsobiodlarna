4e94d26a5b9d43c2f89586af3ff9c0162c222052
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-23 09:06:48 +0200

    lagt till inloggning emd HttpOnly-Cookie och omdirigering till skydda vy

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,50 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import '../pages/Login.scss'
+import { useState } from "react"
+import api from "../services/apiService";
 
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
+  
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+
+      const response = await api.post("/auth/login", {email, password});
+      console.log("Inloggad:", response.data.message);
+      navigate("/mypage");//skickas till skyddad vy mina sidor
+    } catch (err: any) {
+      console.error(err);
+      setError(err.response?.data || "Inloggning misslyckades.");
+    }
+  }
+
   return (
     <div className="login-container">
       <h1>Inloggning</h1>
 
-      <form aria-labelledby="login-form">
+      <form aria-labelledby="login-form" onSubmit={handleSubmit}>
+      {error && <p className="error-message">{error}</p>}
+
         <div className="form-group">
           <label htmlFor="email">E-postadress</label>
-          <input type="email" id="email" name="email" autoComplete="email" required />
+          <input type="email" id="email" name="email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
 
         <div className="form-group">
           <label htmlFor="password">Lösenord</label>
-          <input type="password" id="password" name="password" autoComplete="current-password" required />
+          <input type="password" id="password" name="password" autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)}required />
           <p><NavLink to="/new_password">Glömt lösenord?</NavLink></p>
         </div>
-        <div className="">
+
+        <div className="form-group">
           <button type="submit" className="login-button">Logga in</button>
 
           <div className="register-prompt">
@@ -26,6 +52,7 @@ const Login = () => {
             <p><NavLink to="/register">Registrera dig</NavLink></p>
           </div>
         </div>
+
       </form>
     </div>
   )
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,15 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://visingsobiodlarna-api.azurewebsites.net/api",
-});
-
-//Lägg till token automatiskt om den finns
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  withCredentials:true, //cookies (JWT) skickas automatiskt med varje request
 });
 
 export default api;

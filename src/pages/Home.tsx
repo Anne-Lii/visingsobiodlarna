b9feaeebff4ba879d255d74bb01311011fd35a65
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-04-24 14:06:03 +0200

    lagt till info på home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
-
+import hives from '../assets/bikupor_träd.jpg';
+import '../pages/Home.scss'
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Välkommen till Visingsöbiodlarna!</h1>
+
+      <img className='start-image' src={hives} alt="bikupor under träd på en äng" />
+      <div className="info-text">
+        <p>
+          Visingsöbiodlarna är en ideell förening som samlar öns biodlare för att främja kunskapsutbyte, gemenskap och hållbar biodling. 
+          På Visingsö bedrivs biodling i en unik miljö med närhet till natur och pollinerrika landskap. Vi är för tillfället 17 aktiva biodlare på ön.
+        </p>
+        <p>
+          Föreningen ansvarar även för en godkänd parningsstation för Ligusticaföreningen, vilket ger möjlighet till kontrollerad parning av drottningar i en skyddad zon. 
+          Detta bidrar till friska och starka bisamhällen med goda egenskaper.
+        </p>
+        <p>
+          Genom vår digitala plattform kan medlemmar enkelt hantera sina bigårdar, rapportera data och ta del av nyheter, evenemang och dokument från föreningen.
+        </p>
+      </div>
     </div>
   )
 }
